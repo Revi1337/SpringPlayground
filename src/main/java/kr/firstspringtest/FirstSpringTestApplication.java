18b3c362686780988f845281894fbc68057c88cd
Revi1337 <david122123@gmail.com>
2023-10-09 17:31:58 +0900

    initialize

---
+++ src/main/java/kr/firstspringtest/FirstSpringTestApplication.java
@@ -0,0 +1,13 @@
+package kr.firstspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/firstspringtest/FirstSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.firstspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
